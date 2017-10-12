e11ff1b0ef0fbd14afcc017b0913215b6d14cc71
Matias Aguirre <maguirre@matware.com.ar>
2017-10-12 15:26:02 -0300

    Add 3.7 and 3.8 versions

---
+++ src/Schemas/v38/Contacts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * jUpgradeNext
+ *
+ * @version $Id:
+ * @package jUpgradeNext
+ * @copyright Copyright (C) 2004 - 2016 Matware. All rights reserved.
+ * @author Matias Aguirre
+ * @email maguirre@matware.com.ar
+ * @link http://www.matware.com.ar/
+ * @license GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace JUpgradeNext\Schemas\v40;
+
+use JUpgradeNext\Upgrade\Upgrade;
+
+/**
+ * Upgrade class for Contacts
+ *
+ * This class takes the contacts from the existing site and inserts them into the new site.
+ *
+ * @since	1.0
+ */
+class Contacts extends Upgrade
+{
+}
